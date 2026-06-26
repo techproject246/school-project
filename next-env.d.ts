48f9e64335389f171501c86cb89b79289bcabb67
Vivek Mishra <vmishra9122003@gmail.com>
2026-06-26 18:35:38 +0530

    updated next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="next" />
+/// <reference types="next" />
+/// <reference types="next" />
 /// <reference types="next/image-types/global" />
 import "./.next/dev/types/routes.d.ts";
 
